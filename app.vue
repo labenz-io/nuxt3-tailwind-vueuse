1329bb46431634ba962df94193e3f355ab32668a
gormlabenz <gorm@labenz.io>
2022-12-06 16:59:00 +0100

    added vueuse

--- app.vue
+++ app.vue
@@ -1,3 +1,13 @@
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <h1 ref="header" class="text-3xl font-bold underline">{{ st }}</h1>
 </template>
+
+<script setup>
+const st = ref('hello world')
+
+const { width, height } = useWindowSize()
+
+onMounted(() => {
+  console.log(width.value, height.value)
+})
+</script>
